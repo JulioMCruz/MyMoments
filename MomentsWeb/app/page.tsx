58331218570ac27cd573ca4a2225af4144372897
Julio M Cruz <julio.cruz@eb-ms.net>
2025-03-13 08:17:50 -0400

    Update page.tsx

--- MomentsWeb/app/page.tsx
+++ MomentsWeb/app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
               </div>
               <div className="md:w-1/2">
                 <Image
-                  src="/placeholder.svg?height=400&width=500"
+                  src="/assets/moments-poster.png"
                   alt="Collection of moments"
                   width={500}
                   height={400}
